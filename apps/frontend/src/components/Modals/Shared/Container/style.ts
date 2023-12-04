753992f4236d9193dc12231918e67a69dac7d2a6
Kevin <ysh0327@midasin.com>
2023-12-04 09:33:54 +0900

    feat: code

---
+++ apps/frontend/src/components/Modals/Shared/Container/style.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  box-sizing: border-box;
+  padding: 28px 32px;
+  border: 1px solid #cfd4de;
+  border-radius: 20px;
+`;
